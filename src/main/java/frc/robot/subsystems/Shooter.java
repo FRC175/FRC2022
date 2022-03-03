be950a72f22ff1810c1e87636aa3465f642eb437
JimInput <ojimmy05o@gmail.com>
2022-03-02 19:48:54 -0500

    add shoot calc

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import frc.robot.Constants.ShootConstants;
-
+import frc.robot.commands.Shoot;
 
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
@@ -51,6 +51,35 @@ public class Shooter extends SubsystemBase{
     public void indexerSetOpenLoop(double demand) {
         indexer.set(demand);   
     }
+
+    public enum ShootType {
+        HIGH, LOW, WEAK;
+    }
+
+    public double estimatePower(double distance, String type) {
+        double output;
+        double d1, d2, d3, d4;
+        double RPMupper1, RPMupper2, RPMupper3, RPMupper4;
+        double RPMlower1, RPMlower2, RPMlower3, RPMlower4;
+
+        switch (type) {
+            case "high":
+                // output = RPMupper1 * (distance - d2) / (d1 - d2) * (distance - d3) / (d1 - d3) * (distance - d4) / (d1 - d4) + 
+                // RPMupper2 * (distance - d1) / (d2 - d1) * (distance - d3) / (d2 - d3) * (distance - d4) / (d2 - d4) + 
+                // RPMupper3 * (distance - d1) / (d3 - d1) * (distance - d2) / (d3 - d2) * (distance - d4) / (d3 - d4) + 
+                // RPMupper4 * (distance - d1) / (d4 - d1) * (distance - d2) / (d4 - d2) * (distance - d3) / (d4 - d3);
+            case "low":
+                // output = RPMlower1 * (distance - d2) / (d1 - d2) * (distance - d3) / (d1 - d3) * (distance - d4) / (d1 - d4) + 
+                // RPMlower2 * (distance - d1) / (d2 - d1) * (distance - d3) / (d2 - d3) * (distance - d4) / (d2 - d4) + 
+                // RPMlower3 * (distance - d1) / (d3 - d1) * (distance - d2) / (d3 - d2) * (distance - d4) / (d3 - d4) + 
+                // RPMlower4 * (distance - d1) / (d4 - d1) * (distance - d2) / (d4 - d2) * (distance - d3) / (d4 - d3);
+            default:
+                output = 1500;
+        }
+        return output / 6000;
+    }
+
+    
 
     public void shooterSetOpenLoop(double demand) {
         SmartDashboard.putNumber("Demand", demand);
